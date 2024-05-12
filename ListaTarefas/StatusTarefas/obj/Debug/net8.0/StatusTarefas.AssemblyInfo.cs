1fa2a0cdbdce147e9e8dbe7401afb7ed1916bda3
Rhayanna Paz da Silva Alves <r.paz.da.silva.alves@avanade.com>
2024-05-12 14:56:17 -0300

    carga2

--- ListaTarefas/StatusTarefas/obj/Debug/net8.0/StatusTarefas.AssemblyInfo.cs
+++ ListaTarefas/StatusTarefas/obj/Debug/net8.0/StatusTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StatusTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7690a7ae648805351cb1ca3df4ca2886e32cadf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04327d542c3d62d34ace884207fa8cb1e41d6f06")]
 [assembly: System.Reflection.AssemblyProductAttribute("StatusTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StatusTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
